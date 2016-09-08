dc846ec485883df58f2479e304f883424e70a701
gaotianxiang <gaotianxiang@chunyu.me>
2016-09-08 22:28:08 +0800

    Initial Commit

---
+++ CYGAnalyseTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYGAnalyseTool
+//
+//  Created by 高天翔 on 16/9/8.
+//  Copyright © 2016年 CYGTX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CYGAnalyseTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYGAnalyseTool
+//
+//  Created by 高天翔 on 16/9/8.
+//  Copyright © 2016年 CYGTX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
